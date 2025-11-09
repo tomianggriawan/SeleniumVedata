20866da5c54a8b613b59f53200d7c2e8497c4bff
tomianggriawan <anggriawan.tomi@gmail.com>
2025-11-09 21:52:11 +0700

    add validation

--- src/main/java/org/test/Login.java
+++ src/main/java/org/test/Login.java
@@ -17,6 +17,25 @@ public class Login {
         boolean usernameDisplayed = chrome.findElement(By.xpath("//*[text()='Username']")).isDisplayed();
         System.out.println("Username: " + usernameDisplayed);
 
+        boolean usernamePlaceHolderDisplayed = chrome.findElement(By.xpath("//input[@placeholder = 'Masukan username']")).isDisplayed();
+        System.out.println("Username placeholder: " + usernamePlaceHolderDisplayed);
+
+        boolean passwordDisplayed = chrome.findElement(By.xpath("//*[text()='Password']")).isDisplayed();
+        System.out.println("Password: " + passwordDisplayed);
+
+        boolean passwordPlaceHolderDisplayed = chrome.findElement(By.xpath("//input[@placeholder = 'Masukan password']")).isDisplayed();
+        System.out.println("Password placeholder: " + passwordPlaceHolderDisplayed);
+
+        chrome.findElement(By.id("input-0")).click();
+        chrome.findElement(By.id("input-2")).click();
+
+        boolean emailValidation = chrome.findElement(By.xpath("//*[text()='E-mail wajib diisi']")).isDisplayed();
+        System.out.println("Password Validation: " + emailValidation);
+
+        chrome.findElement(By.tagName("input")).click();
+
+        boolean passwordValidation = chrome.findElement(By.xpath("//*[text()='Password wajib diisi']")).isDisplayed();
+        System.out.println("Email Validation: " + passwordValidation);
 
         String title = chrome.getTitle();
         if (title.equals("VEDATA")){
@@ -25,6 +44,6 @@ public class Login {
             System.out.println("TEST FAILED");
         }
 
-        chrome.close();
+//        chrome.close();
     }
 }
